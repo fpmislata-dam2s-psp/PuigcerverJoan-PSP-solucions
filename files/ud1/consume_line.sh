a0f51d94568fd6be4d66466a71e3b6b9527cb0d1
joapuiib <joapuiib@gmail.com>
2024-04-09 17:48:54 +0200

    files ud1 practices

---
+++ files/ud1/consume_line.sh
@@ -0,0 +1,14 @@
+FILENAME=$1
+i=0
+DELAY=1
+
+if [ $# -gt 1 ]; then
+    DELAY=$2
+fi
+
+cat $FILENAME | while read line
+do
+    echo "Process $$ - Line $i: $line"
+    ((i++))
+    sleep $DELAY
+done
--- src/main/java/ud1/exercises/ConsumeLineMultiProcess.java
+++ src/main/java/ud1/exercises/ConsumeLineMultiProcess.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 
 public class ConsumeLineMultiProcess {
     public static void main(String[] args) {
-        ProcessBuilder pbColors = new ProcessBuilder("wsl", "files/ud1/consume_line.sh", "files/ud1/colors.txt", "1").inheritIO();
+        ProcessBuilder pbColors = new ProcessBuilder("wsl", "files/ud1/consume_line.sh", "files/ud1/colors.txt", "3").inheritIO();
         ProcessBuilder pbConcessionari = new ProcessBuilder("wsl", "files/ud1/consume_line.sh", "files/ud1/concessionari.csv", "1").inheritIO();
+        ProcessBuilder pbLorem = new ProcessBuilder("wsl", "files/ud1/consume_line.sh", "files/ud1/lorem.txt", "2").inheritIO();
 
         try {
             Process processColors = pbColors.start();
             Process processConcessionari = pbConcessionari.start();
+            Process processLorem = pbLorem.start();
 
             processColors.waitFor();
             processConcessionari.waitFor();
+            processLorem.waitFor();
 
             System.out.println("Tots els processos han acabat.");
         } catch (IOException | InterruptedException e) {
